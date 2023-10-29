3f31eb1f796a2fe54694b62af365a22a6f883717
andrejsandzans <149305268+andrejsandzans@users.noreply.github.com>
2023-10-29 15:05:54 +0200

    Add files via upload

--- z/script.js
+++ z/script.js
@@ -1,29 +1,37 @@
-    // Массив с URL изображений, которые вы хотите листать
     var images = [
         'src/image1.jpg',
         'src/image2.jpg',
         'src/image3.jpg'
     ];
     
-    var currentImageIndex = 0; // Индекс текущего изображения
+    var currentImageIndex = 0;
     
-    // Функция для обновления изображения на странице
     function updateImage() {
         var imageElement = document.querySelector('.background-image img');
         imageElement.src = images[currentImageIndex];
     }
     
-    // Обработчик клика на кнопку "←" (предыдущее изображение)
     document.getElementById('prev-button').addEventListener('click', function() {
         currentImageIndex = (currentImageIndex - 1 + images.length) % images.length;
         updateImage();
     });
     
-    // Обработчик клика на кнопку "→" (следующее изображение)
     document.getElementById('next-button').addEventListener('click', function() {
         currentImageIndex = (currentImageIndex + 1) % images.length;
         updateImage();
     });
     
-    // Инициализация изображения
     updateImage();
+
+    document.addEventListener("DOMContentLoaded", function () {
+
+        const header = document.querySelector("header");
+
+        const colorToggleButton = document.getElementById("color-toggle-button");
+    
+        colorToggleButton.addEventListener("click", function () {
+        const randomColor = "#" + Math.floor(Math.random() * 16777215).toString(16);
+        header.style.backgroundColor = randomColor;
+        });
+    });
+  
